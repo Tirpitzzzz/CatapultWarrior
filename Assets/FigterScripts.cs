b0770eda98406464f3cf4d4e532036164cefb5d2
Tirpitzzz <1100012943@pku.edu.cn>
2019-12-26 22:19:30 +0800

    first commit

---
+++ Assets/FigterScripts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighter : MonoBehaviour
+{
+
+    public Vector3 screenPos;
+    public Vector3 offset;
+    void OnMouseDown() {
+        screenPos = Camera.main.WorldToScreenPoint(transform.position);  
+        offset = screenPos - Input.mousePosition;
+    }
+    void OnMouseDrag() {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + offset);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public Vector3 screenPos;
+    public Vector3 offset;
+    void OnMouseDown() 
+    {
+        screenPos = Camera.main.WorldToScreenPoint(transform.position);  
+        offset = screenPos - Input.mousePosition;
+    }
+    void OnMouseDrag() 
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + offset);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
